c25d3abd7ca3a903d60e8977df6c383a7bc8c997
Agostina Romero <agostinaromontamatt@gmail.com>
2021-11-25 01:24:38 -0300

    cambios

--- index.js
+++ index.js
@@ -1,67 +1,21 @@
-const express = require('express');
-const fs = require('fs')
+const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 8080;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+const productsRoutes = require("./routes/products");
+const port = 8081;
 
-// Rutas
-app.get('/', (req, res) => {
-    res.send(`<h1>Mi servidor express</h1>`)
-})
+// Middleware o lógica de intercambio de información entre aplicaciones
+// Routes
+app.use("/static", express.static(__dirname + "/public"));
+app.use("/api/products", productsRoutes);
 
+app.get("/form", (req, res) => {
+  res.sendFile(__dirname + "/public/form.html");
+});
 
-// lectura del archivo
-const readFileAsync = async (path) =>{
-    try{
-        let file = await fs.promises.readFile(path, 'utf-8');
-        return file;
-    }
-    catch(err){
-        throw new Error('Error de lectura!..')
-    }
-}
-
-class Contenedor {
-    constructor(){
-        this.producto = []
-    }
-
-    async getAll(path){
-        let data = await readFileAsync(path);
-
-        if (data){
-            let dataFile = JSON.parse(data);
-            // this.producto = [...dataFile];
-        
-        // Me devuelve un array con los productos disponibles en el server
-        app.get('/productos', (req, res) => {
-            res.json(dataFile)
-        })
-
-        // Me devuelve un producto al azar
-        app.get('/productoRandom', (req, res) => {
-            let productRandom = dataFile[Math.floor(Math.random() * dataFile.length)]
-            res.json(productRandom)
-        })
-
-        }
-    }
-}
-
-let ListProducts = new Contenedor();
-ListProducts.getAll("./productos.txt")
-
-
-
-
-
-// listen port
 app.listen(port, () => {
-    console.log('Server run on port ' + port)
-})
-
-
-
-
-
+  console.log("server run on port " + port);
+});
